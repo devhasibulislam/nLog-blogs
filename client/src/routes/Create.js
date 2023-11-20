156cdfa28f144f29ddb367b4791bf12d7a92254c
devhasibulislam <devhasibulislam@gmail.com>
2023-11-20 19:21:02 +0600

    create route init

---
+++ client/src/routes/Create.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreatePost from "../components/Form/CreatePost";
+
+const Create = () => {
+  return (
+    <section className={`flex h-screen w-screen justify-center items-center`}>
+      <CreatePost />
+    </section>
+  );
+};
+
+export default Create;
