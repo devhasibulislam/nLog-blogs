7c93d543d6dbebb8defd20bb85fe8a1aefceb50a
Hasibul Islam <devhasibulislam@gmail.com>
2024-01-14 22:12:48 +0600

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://nlog-blogs-ssr.vercel.app";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
